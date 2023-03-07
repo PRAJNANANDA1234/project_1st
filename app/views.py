c7573ae1f906af6850307d2970c940c535fd07dd
PRAJNANANDA1234 <sonupinky393@gmail.com>
2023-03-07 05:42:51 -0800

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def Puri(request):
+    return HttpResponse('Puri is famous for jagannath temple')
